f06aec0a97b96559002a02920a12fa5ad32e52b4
Anton <anton.shever@faceit-team.com>
2019-01-08 18:21:28 +0200

    namespace

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,6 @@
 <?php
-Route::group(['prefix' => 'manager-laravel'], function () {
-    $controllers = 'Aqjw\ManagerLaravel\Controllers\\';
-
-    Route::get('', $controllers . 'DashboardController@index')->name('managerl.home');
-    Route::get('models', $controllers . 'ModelsController@index')->name('managerl.models');
-    Route::get('models/{name}', $controllers . 'ModelsController@view')->name('managerl.models.view');
+Route::group(['prefix' => 'manager-laravel', 'namespace' => 'Aqjw\ManagerLaravel\Controllers'], function () {
+    Route::get('', 'DashboardController@index')->name('managerl.home');
+    Route::get('models', 'ModelsController@index')->name('managerl.models');
+    Route::get('models/{name}', 'ModelsController@view')->name('managerl.models.view');
 });
